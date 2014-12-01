b525af023247440f6b002a246926dc5af6fbd0f2
StellaJiang <wanting.jiang88@gmail.com>
2014-11-30 22:21:47 -0800

    add class User
    add class User

---
+++ bbq-db-project/src/main/java/com/bbq/db/project/model/User.java
@@ -0,0 +1,71 @@
+package com.bbq.db.project.model;
+
+import java.sql.Date;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String password;
+	private String gender;
+	private String email;
+	private String telephone;
+	private Date registerTime;
+	public int getUserId() {
+		return userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public Date getRegisterTime() {
+		return registerTime;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public void setRegisterTime(Date registerTime) {
+		this.registerTime = registerTime;
+	}
+	public User(int userId, String userName, String password, String gender,
+			String email, String telephone, Date registerTime) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.gender = gender;
+		this.email = email;
+		this.telephone = telephone;
+		this.registerTime = registerTime;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
